64234ff3d48546877be87212ac9a20d9c486ac7a
NeuroPhD <151127158+plitvinphd@users.noreply.github.com>
2024-10-15 02:35:35 -0500

    Update main.py v5

--- main.py
+++ main.py
@@ -11,6 +11,10 @@ import base64
 
 app = FastAPI()
 
+if __name__ == "__main__":
+    import uvicorn
+    port = int(os.environ.get("PORT", 8000))  # Use the PORT environment variable
+    uvicorn.run("main:app", host="0.0.0.0", port=port)
 # Load environment variables
 load_dotenv()
 
